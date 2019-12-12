afbfcc5eff835bac4f56b066c9d0d3f1410f7ec7
sky9023 <25349023.qq@gmail.com>
2019-12-12 22:50:07 +0800

    initial commit

---
+++ source/chain_reaction.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "../include/board.h"
+#include "../include/player.h"
+#include "../include/rules.h"
+#include "../include/algorithm.h"
+
+using namespace std;
+using namespace std::chrono_literals;
+
+int main(){
+
+    Board board;
+    Player red_player(RED);
+    Player blue_player(BLUE);
+
+    bool first_two_step = true;
+    int round = 1;
+    int index[2];
+
+    while(1){
+
+        //////////// Red Player operations ////////////
+        // algorithm_A(board, red_player, index);
+        cout << "input the index:  ";
+        cin >> index[0] >> index[1];
+        board.place_orb(index[0], index[1], &red_player);
+
+        if(rules_violation(red_player)) return 0;
+
+        board.print_current_board(index[0], index[1], round);
+        round++;
+        
+        // this_thread::sleep_for(1000ms);
+        system("Pause");
+
+        if(board.win_the_game(red_player) && !first_two_step){
+            cout << "Red Player won the game !!!" << endl;
+            return 0;
+        }
+
+        //////////// Blue Player operations ////////////
+        algorithm_B(board, blue_player, index);
+        board.place_orb(index[0], index[1], &blue_player);
+
+        if(rules_violation(blue_player)) return 0;
+        
+        board.print_current_board(index[0], index[1], round);
+        round++;
+        
+        //this_thread::sleep_for(1000ms);
+        system("Pause");
+
+        if(board.win_the_game(blue_player) && !first_two_step){
+            cout << "Blue Player won the game !!!" << endl;
+            return 0;
+        }
+
+        first_two_step = false;
+    }
+
+    return 0;
+} 
